a6e118b90be04a93c025305b916d4afbbab096c8
PopiT4 <tbsitu@gmail.com>
2015-04-23 13:29:01 +0200

    Implemented parallel map

--- LabB/part1repastock.hs
+++ LabB/part1repastock.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 -- | an example of how repa and our extention can be used together
-module Part1 where
+--module Part1 where
 import Data.List
 import qualified Data.Array.Repa as R
 
-type IntVec = R.Array R.U R.DIM1 [Int]
+type IntVec = R.Array R.U R.DIM1 Int
 type Index = Int
 {-
 runRepaStock :: Int
@@ -31,14 +32,20 @@ stock (l:ls) = max (stock ls) (calc ls l 0)
         calc (l:[]) x y = max (l-x) y  
         calc (l:ls) x y = max (l-x) (calc ls x y) 
 -}      
-        
+
+main :: IO ()
+main = pstock [1..30000]
+
+
 --pstock::[Int]->IO ()
 pstock ls = do
     let i = length ls
     let l = (tails (ls::[Int])) ::[[Int]]
-    let (x,y) = getMaxIndex $ R.toList (R.map runOnTails2 
-                (R.fromListUnboxed (R.Z R.:. (i::Int)) l :: IntVec))
-    let finIn = snd x+y
+    (test :: R.Array R.U R.DIM1 (Int, Int)) <- R.computeP ((R.map (runOnTails2 ls)
+                (R.fromListUnboxed (R.Z R.:. (i+1::Int)) [0..i] :: IntVec))
+                :: R.Array R.D R.DIM1 (Int, Int) )  
+    let (x,y) = getMaxIndex $ R.toList test
+    let finIn = snd x+y-1
     --let repLs = R.fromListUnboxed (R.ix1 i) l ::(R.Array R.U R.DIM1 [Int])  
     putStrLn $ show (y, finIn, fst x)
 
@@ -54,10 +61,19 @@ compare2 b1 b2
   | b1 < b2 = LT
   | b1 == b2 = EQ
 
-runOnTails2::[Int]->[Int]->(Int,Int)
-runOnTails2 [] prices = (0,0)
-runOnTails2 (l:ls) prices = helper l ls 1 0 0 
-        
+runOnTails2::[Int]->Int->(Int,Int)
+runOnTails2 prices index = helper (prices !! index) (drop index prices) 1 0 0
+
+helper2 index prices i mi m = helper
+
+{-
+helper2::Int->[Int]->Int->Int->Int->(Int,Int)
+helper2 _ [] _ _ _  = (0,0)
+helper2 f (l:[]) i mi m | l-f > m = (l-f, i)
+                       | otherwise = (m, mi)
+helper2 f (l:ls) i mi m | l-f > m = helper f ls (i+1) i (l-f)
+                       | otherwise = helper f ls (i+1) mi (m)
+-}
 runOnTails::[Int]->(Int,Int)
 runOnTails [] = (0,0)
 runOnTails (l:ls) = helper l ls 1 0 0 
